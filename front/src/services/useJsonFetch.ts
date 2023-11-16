7fe6914248d69eb083318d9d88e348452d18f502
Niko Ivan <nixonivan@rambler.ru>
2023-11-16 22:03:27 +0300

    task done

---
+++ front/src/services/useJsonFetch.ts
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from "react";
+
+export default async function useJsonFetch(url: string) {
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [hasError, setError] = useState<boolean>(false);
+  const timestampRef = useRef();
+
+  useEffect(() => {
+    (async () => {
+      const timestamp = Date.now();
+      timestampRef.current = timestamp;
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          setError(true);
+          throw new Error(response.statusText);
+        }
+        if (!/^20./.test(`${response.status}`)) {
+          setError(true);
+        }
+
+        const json = await response.json();
+        if (!json) {
+          setError(true);
+        }
+        if (timestampRef.current === timestamp) {
+          setData(json);
+        }
+      } catch (e) {
+        setError(true);
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
+  return [data, isLoading, hasError];
+}
