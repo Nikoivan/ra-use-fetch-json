f98e0c725e9713fd98ce561bf5ba00572fec62be
Niko Ivan <nixonivan@rambler.ru>
2023-11-17 21:48:45 +0300

    task full done

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 
 import Data from "./componets/Data/Data";
@@ -6,26 +5,6 @@ import ErrorData from "./componets/ErrorData/ErrorData";
 import LoadingData from "./componets/LoadingData/LoadingData";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  // async function Ok() {
-  //   const test = await useJsonFetch("http://localhost:7070/data");
-  //   console.log(test);
-  // }
-
-  // async function Err() {
-  //   const test = await useJsonFetch("http://localhost:7070/error");
-  //   console.log(test);
-  // }
-
-  // async function Load() {
-  //   const test = await useJsonFetch("http://localhost:7070/loading");
-  //   console.log(test);
-  // }
-
-  // Err();
-  // Load();
-
   return (
     <>
       <Data />
